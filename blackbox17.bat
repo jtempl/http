3ae88e3f867061686259ed62752bd7f590ba32e5
Josef Templ <josef.templ@gmail.com>
2018-12-09 11:16:00 +0100

    bug fix: hard-coded host names removed in TestHttpClient and TestHttpsClient, docu improved

--- blackbox17.bat
+++ blackbox17.bat
@@ -1,3 +1,3 @@
-start /b C:\Programme\"BlackBox Component Builder 1.7.1"\BlackBox.exe /USE . /NOSCROLL
+start C:\"Program Files (x86)"\"BlackBox Component Builder 1.7.1"\BlackBoxF6.exe /USE . 
 
 rem see command line reference under: https://technet.microsoft.com/de-de/library/cc770297(v=ws.10).aspx
